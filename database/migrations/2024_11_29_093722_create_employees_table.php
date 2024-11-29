9e246cbc72bbd5d7eb425de198cac1ae2428dce9
Shamee <bajrasami7@gmail.com>
2024-11-29 17:18:28 +0545

    all tables created

---
+++ database/migrations/2024_11_29_093722_create_employees_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->enum('gender',['M','F','O']);
+            $table->date('join_date');
+            $table->foreignId('role_id')
+                  ->constrained('roles')
+                  ->onDelete('cascade')
+                  ->onUpdate('cascade')
+                  ->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
---
+++ database/migrations/2024_11_29_093821_create_vehicles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->id();
+            $table->varchar('number_plate');
+            $table->json('vehicle_info');
+            $table->foreignId('employee_id')
+                 ->constrained('employees')
+                 ->onDelete('cascade')
+                 ->onUpdate('cascade')
+                 ->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vehicles');
+    }
+};
